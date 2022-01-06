155a831153e779c1a1a0854f1750d11c780a039c
Roman120709 <urall76@yandex.ru>
2022-01-06 14:40:00 +0300

    Commit1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int priceMiles = 20;
+                int miles;
+        miles = cost / priceMiles;
+        return miles;
+    }
+    }
+
+
+
